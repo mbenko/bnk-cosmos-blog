fa131af3b1f74fbac8d3bcdbee42d854b40d4cc2
Mike <mbenko@benko.com>
2024-09-28 12:40:46 -0500

    Add setup script

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+# Setup
+git remote add origin https://github.com/mbenko/bnk-cosmos-blog.git
+git branch -M main
+git push -u origin main
+
+
+devenv .\bnk-cosmos-blog.sln
+
+start Powerpnt.exe docs/BNK22-Cosmos-blog.pptx
