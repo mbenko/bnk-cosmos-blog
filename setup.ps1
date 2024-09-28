58d0cb64025da551661b5a4e91cb23f55aa0012d
Dev Dad <devdad@benko.com>
2024-09-28 14:08:46 -0500

    Update setup for events

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,15 @@
 # Setup
-git remote add origin https://github.com/mbenko/bnk-cosmos-blog.git
-git branch -M main
-git push -u origin main
+# git init
+# git remote add origin https://github.com/mbenko/bnk-cosmos-blog.git
+
+# Create event branch
+$eventName = "sqlsat24"
+git branch -M $eventName
 
 
 devenv .\bnk-cosmos-blog.sln
 
 start Powerpnt.exe docs/BNK22-Cosmos-blog.pptx
+
+git add .
+git push -u origin $eventName
